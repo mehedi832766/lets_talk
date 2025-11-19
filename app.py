28644fb2823fcac7dc3d30627ac75a8ddb7fee81
Mehedi Hasan <74487648+mehedi832766@users.noreply.github.com>
2025-11-19 23:55:13 +0600

    Refactor app.py for object detection functionality

--- app.py
+++ app.py
@@ -182,8 +182,9 @@ def get_word():
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+# if __name__ == '__main__':
+#     app.run(host="0.0.0.0", port=5000)
     #http://localhost:8000/video for video source
     #http://localhost:8000 for image source
+
 
